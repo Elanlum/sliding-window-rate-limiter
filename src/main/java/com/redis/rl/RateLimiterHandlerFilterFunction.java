a793d03ec0cefcebaa80619a136ff545bd5be8c9
Roman Gorbatenko <gorbatenko.r@gmail.com>
2023-01-13 15:06:22 +0300

    fix addition request to storage if it is out of limits

--- src/main/java/com/redis/rl/RateLimiterHandlerFilterFunction.java
+++ src/main/java/com/redis/rl/RateLimiterHandlerFilterFunction.java
@@ -7,7 +7,7 @@ import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-import reactor.util.function.Tuple3;
+import reactor.util.function.Tuple2;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -29,35 +29,33 @@ public class RateLimiterHandlerFilterFunction implements HandlerFilterFunction<S
     @Override
     public Mono<ServerResponse> filter(ServerRequest request, HandlerFunction<ServerResponse> next) {
         String key = String.format("rate_limit_%s", requestAddress(request.remoteAddress()));
+        ByteBuffer bbKey = ByteBuffer.wrap(key.getBytes());
 
-        Mono<List<ByteBuffer>> mono = function(key);
-
-        return mono.flatMap(a -> {
-            if (a.size() > MAX_REQUESTS_PER_MINUTE) {
-                return ServerResponse.status(TOO_MANY_REQUESTS).build();
-            }
-            return next.handle(request);
-        });
+        return checkRequest(bbKey, request, next);
     }
 
-    private Mono<List<ByteBuffer>> function(String key) {
-
+    private Mono<ServerResponse> checkRequest(ByteBuffer bbKey, ServerRequest request, HandlerFunction<ServerResponse> next) {
         return redisTemplate.createMono(connection -> {
             long currentTime = System.currentTimeMillis();
             long slidingWindowTime = 60000L;
 
-            ByteBuffer bbKey = ByteBuffer.wrap(key.getBytes());
+            Mono<Long> removeByScore = connection.zSetCommands().zRemRangeByScore(bbKey, Range.from(Range.Bound.inclusive(0.0))
+                    .to(Range.Bound.inclusive(Double.parseDouble(String.valueOf(currentTime)) - Double.parseDouble(String.valueOf(slidingWindowTime)))));
 
-            Mono<Tuple3<Long, Long, Boolean>> zip = Mono.zip(
-                    connection.zSetCommands().zRemRangeByScore(bbKey, Range.from(Range.Bound.inclusive(0.0))
-                            .to(Range.Bound.inclusive(Double.parseDouble(String.valueOf(currentTime)) - Double.parseDouble(String.valueOf(slidingWindowTime))))),
-                    connection.zSetCommands().zAdd(bbKey, Double.valueOf(String.valueOf(currentTime)), ByteBuffer.wrap(Long.toString(currentTime).getBytes())),
-                    connection.keyCommands().expire(bbKey, Duration.ofMillis(currentTime + slidingWindowTime))
-            );
-            Mono<List<ByteBuffer>> requests = connection.zSetCommands()
-                    .zRange(bbKey, Range.from(Range.Bound.inclusive(0L)).to(Range.Bound.inclusive(-1L))).collectList();
+            Mono<List<ByteBuffer>> countRequests = removeByScore.then(connection.zSetCommands()
+                    .zRange(bbKey, Range.from(Range.Bound.inclusive(0L)).to(Range.Bound.inclusive(-1L))).collectList());
 
-            return zip.then(requests);
+            return countRequests.flatMap(list -> {
+                if (list.size() >= MAX_REQUESTS_PER_MINUTE) {
+                    return ServerResponse.status(TOO_MANY_REQUESTS).build();
+                }
+
+                Mono<Tuple2<Long, Boolean>> zip = Mono.zip(
+                        connection.zSetCommands().zAdd(bbKey, Double.valueOf(String.valueOf(currentTime)), ByteBuffer.wrap(Long.toString(currentTime).getBytes())),
+                        connection.keyCommands().expire(bbKey, Duration.ofMillis(currentTime + slidingWindowTime))
+                );
+                return zip.then(next.handle(request));
+            });
         });
     }
 
